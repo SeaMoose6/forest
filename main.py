3199e22bf80a7b3aa98e59518b9f987220d19eb0
SeaMoose6 <tophtoph05@gmail.com>
2021-10-19 13:59:06 -0400

    moon

--- main.py
+++ main.py
@@ -10,13 +10,19 @@ pygame.init()
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
 BLACK = (0, 0, 0)
-GROUND_BLACK = (23, 24, 28)
+GROUND_BLACK = (13, 14, 20)
 BACK_BLACK = (5, 5, 5)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
-SKY = (8, 37, 112)
+SKY = (5, 5, 36)
 FOG = pygame.Color(206, 209, 222, 50)
+EYES = (125, 19, 11)
+color_list = [EYES, GROUND_BLACK, GROUND_BLACK, GROUND_BLACK, GROUND_BLACK, GROUND_BLACK, GROUND_BLACK,]
 #pygame.Color.update(FOG, 206, 209, 222, 50)
+x_left = 10
+x_right = 10
+y_up = 10
+y_down = 10
 
 
 PI = math.pi
@@ -111,9 +117,55 @@ class Bat:
         pygame.draw.circle(screen, RED,
                            (self.x+95, self.y-35), 4)
     def move_bat(self):
-        self.y-=3
-        if self.y <= 0:
+        self.y-=5
+        if self.y <= -1000:
             self.y = 1000
+
+class Eyes:
+    def __init__(self, x, y, color, move):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.move = move
+
+    def draw_eyes(self):
+        pygame.draw.circle(screen, self.color,
+                           (self.x-10, self.y), 5)
+        pygame.draw.circle(screen, self.color,
+                           (self.x+10, self.y), 5)
+    def move_eyes_right(self):
+        for num in range(100):
+            if num == 99:
+                self.color = random.choice(color_list)
+        self.x+=self.move
+        if self.x>=950:
+            self.move*=-1
+        if self.x<=600:
+            self.move*=-1
+        self.y+=self.move
+        if self.x>=850:
+            self.move*=-1
+        if self.x<=450:
+            self.move*=-1
+class Moon:
+    def __init__(self, x, y, speed):
+        self.x = x
+        self.y = y
+        self.speed = speed
+
+    def draw_moon(self):
+        pygame.draw.circle(screen, WHITE, (self.x, self.y), 50)
+
+    def move_moon(self):
+        self.y -= self.speed
+        self.x += self.speed/2
+        if self.y <= 50:
+            self.speed = 0
+
+
+
+
+
 pygame.init()
 
 screen = pygame.display.set_mode(SIZE)
@@ -140,6 +192,10 @@ fog = Fog(200, 300, FOG)
 
 bat = Bat(400, 1000)
 
+eyes = Eyes(650, 700, EYES, 5)
+
+moon = Moon(200, 300, 1)
+
 
 while running:
     for event in pygame.event.get():
@@ -148,6 +204,12 @@ while running:
 
     screen.fill(SKY)
     pygame.draw.rect(screen, GROUND_BLACK, [0, 450, 1000, 800])
+
+    #eyes.draw_eyes()
+    #eyes.move_eyes_right()
+
+    moon.draw_moon()
+    moon.move_moon()
 
     for tree in background_trees1:
         tree.draw_tree()
