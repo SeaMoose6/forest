3f1e82f73d8c0b9bd54c2c531ea24d9dcb0fd759
SeaMoose6 <tophtoph05@gmail.com>
2021-11-10 09:20:37 -0500

    images

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import pygame
 import math
 import random
-
+##################################################
+''''
+At the beginning you will notice a 8-bit bat that you can control with your mouse. 
+The goal of the game is to catch as many falling fireflies as possible while also avoiding
+the poison berries that will fall along side and give -5 points. You will hear a crunch sound each time you 
+eat a fly. And once 30 seconds are up, GAME OVER will be printed on the screen and 
+your final score will be displayed. My best is 92.
+Sometimes when run it will say "berry" is not defined but if you just try running it 
+again it should work. 
+'''
+####################################################
 #in terminal "pip install pygame"
 
 pygame.init()
@@ -38,30 +48,33 @@ FPS = 60
 
 # functions
 class Tree:
-    def __init__(self, x, y, color, width=0):
+    def __init__(self, display, x, y, color, img, width=0):
+        self.display = display
         self.x = x
         self.y = y
         self.color = color
+        self.img = img
         self.bat_width = width
 
     def draw_tree(self):
-        pygame.draw.polygon(screen, self.color,
-                            [(self.x, self.y+75),
-                             (self.x+75, self.y),
-                             (self.x+150, self.y+75)])
-        pygame.draw.polygon(screen, self.color,
-                            [(self.x, self.y+125),
-                             (self.x+75, self.y+50),
-                             (self.x+150, self.y+125)])
-        pygame.draw.polygon(screen, self.color,
-                            [(self.x, self.y+175),
-                             (self.x+75, self.y+100),
-                             (self.x+150, self.y+175)])
-        pygame.draw.polygon(screen, self.color,
-                         [(self.x+50, self.y+175),
-                          (self.x+100, self.y+175),
-                          (self.x+100, self.y+675),
-                          (self.x+50, self.y+675)])
+        self.display.blit(self.img, [self.x, self.y])
+        # pygame.draw.polygon(screen, self.color,
+        #                     [(self.x, self.y+75),
+        #                      (self.x+75, self.y),
+        #                      (self.x+150, self.y+75)])
+        # pygame.draw.polygon(screen, self.color,
+        #                     [(self.x, self.y+125),
+        #                      (self.x+75, self.y+50),
+        #                      (self.x+150, self.y+125)])
+        # pygame.draw.polygon(screen, self.color,
+        #                     [(self.x, self.y+175),
+        #                      (self.x+75, self.y+100),
+        #                      (self.x+150, self.y+175)])
+        # pygame.draw.polygon(screen, self.color,
+        #                  [(self.x+50, self.y+175),
+        #                   (self.x+100, self.y+175),
+        #                   (self.x+100, self.y+675),
+        #                   (self.x+50, self.y+675)])
 
 class Bat:
     def __init__(self, display, x, y, width, score_add, score_sub, img, score=0):
@@ -124,6 +137,7 @@ class Bat:
 
             if other == fly:
                 self.score += self.score_add
+                crunch_sound.play()
                 other.x = random.randint(0, 1000)
                 other.y = 0
                 print(self.score)
@@ -200,15 +214,13 @@ class Score:
         screen.blit(text, (50, 25))
 
     def draw_time(self):
-        timer = font.render(f"Time = {self.time}", True, WHITE)
+        timer = font.render(f"Time = {self.time/1000}", True, WHITE)
         screen.blit(timer, (50, 50))
 
     def draw_game_over(self):
         screen.fill(SKY)
         game_over = font2.render("GAME OVER", True, RED)
         screen.blit(game_over, (150, 300))
-
-
 
 class Moon:
     def __init__(self, x, y, speed):
@@ -236,23 +248,18 @@ pygame.display.set_caption("The Forest")
 bat_img = pygame.image.load("oie_mQ18gW90tuKu.png")
 game_over_sound = pygame.mixer.Sound("game_over_bad_chest.wav")
 background_img = pygame.image.load("oie_8153311knEW6vqY.png")
-
+crunch_sound = pygame.mixer.Sound("crunch.1.ogg")
+tree_img = pygame.image.load("oie_101454546xf2O7k6.png")
 
 clock = pygame.time.Clock()
 
 running = True
 
 
-trees1 = [Tree(random.randint(-100, 250), random.randint(0, 200), BLACK)
+trees1 = [Tree(screen, random.randint(-100, 250), random.randint(0, 200), BLACK, tree_img)
          for num in range(5)]
 
-background_trees1 = [Tree(random.randint(-100, 250), random.randint(0, 100), BACK_BLACK)
-         for num in range(5)]
-
-trees2 = [Tree(random.randint(600, 1000), random.randint(0, 200), BLACK)
-         for num in range(5)]
-
-background_trees2 = [Tree(random.randint(600, 1000), random.randint(0, 100), BACK_BLACK)
+trees2 = [Tree(screen, random.randint(600, 1000), random.randint(0, 200), BLACK, tree_img)
          for num in range(5)]
 
 bat = Bat(screen, 400, 1000,175, 1, 5, bat_img)
@@ -288,21 +295,11 @@ while running:
             running = False
 
     screen.blit(background_img, [0, 0])
-    # pygame.draw.rect(screen, GROUND_BLACK, [0, 450, 1000, 800])
     score = Score(pygame.time.get_ticks())
-    #eyes.draw_eyes()
-    #eyes.move_eyes_right()
 
-    #moon.draw_moon()
-    #moon.move_moon()
-
-    for tree in background_trees1:
-        tree.draw_tree()
     for tree in trees1:
         tree.draw_tree()
 
-    for tree in background_trees2:
-        tree.draw_tree()
     for tree in trees2:
         tree.draw_tree()
 
@@ -329,7 +326,7 @@ while running:
         screen.blit(timer, (50, 50))
         bat.score_add = 0
         bat.score_sub = 0
-        score.time = 30000
+        score.time = 30000/1000
 
 
     pygame.display.flip()
